006808f48cfb00a4e5c0cbd3ebec7d5175c15986
Tailane2517 <tailanesantos1207@hotmail.com>
2025-03-14 03:17:26 +0000

    Atividade pontuada

--- Atividade pontuada.py/Questão 2.py
+++ Atividade pontuada.py/Questão 2.py
@@ -6,16 +6,43 @@ nome = input("Digite o seu nome: ")
 sexo = input("Digite o seu sexo (M/F): ").upper()
 estado_civil = input("Digite o seu estado civil (SOLTEIRO(A)/CASADO(A)/DIVORCIADO(A)/VIÚVO(A)): ").upper()
 
+
 if sexo == "F" and estado_civil == "CASADA":
-    tempo_casada = int(input("Digite o tempo de casada (anos): "))
-    
+    tempo_casada = int(input("Digite o Tempo de casada (anos): "))
+     
+
 print("\nDados do usuário")   
 print(f"Nome:", {nome})
-print(f"Sexo:", {nome})
+print(f"Sexo:", {sexo})
 print(f"Estado Civil:", {estado_civil})
 
-if tempo_casada:
-    print(f"Tempo de Casada(anos):{tempo_casada}")
+
+  
+print("Nome:",nome )
+print("Sexo:",sexo)
+print("Estado civil:", estado_civil)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+   
 
 
                                     
